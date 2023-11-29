57efb48068b27b64726ed041a04b54a7acb00576
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-29 10:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(str){
+	let obj = {};
+	let arr = str.split('');
+	for(let i = 0; i < arr.length; i++){
+		obj[arr[i]] = [];
+	}
+	for(let key in obj){
+		for(let i = 0; i < arr.length; i++){
+			if(arr[i] == key){
+				obj[key].push(i);
+			}
+		}
+	}
+	return obj;
+}
